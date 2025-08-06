766b7e185be49f9c9516860322f9a197ca0b30e6
JuanaNasi <juanitanasi@gmail.com>
2025-08-06 17:47:25 -0300

    Termino de subir las modificaciones

--- firmware/projects/0_blinking/main/0_blinking.c
+++ firmware/projects/0_blinking/main/0_blinking.c
@@ -10,7 +10,7 @@
  * |:----------:|:-----------------------------------------------|
  * | 12/09/2023 | Document creation		                         |
  *
- * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ * @author Juana Nasi (juananasi3009@gmail.com)
  *
  */
 
@@ -30,11 +30,19 @@
 void app_main(void){
     LedsInit();
     while(true){
-        printf("LED ON\n");
+        printf("LED ON 1\n");
         LedOn(LED_1);
+        printf("LED ON 2\n");
+        LedOn(LED_2);
+        printf("LED ON 3\n");
+        LedOn(LED_3);
         vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
-        printf("LED OFF\n");
+        printf("LED OFF 2\n");
         LedOff(LED_1);
+        printf("LED OFF 2\n");
+        LedOff(LED_2);
+         printf("LED OFF 3\n");
+        LedOff(LED_3);
         vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
     }
 }
